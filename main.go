a13cc89b1b5abd56e50f3b2f74bbd9662b523c54
AndrewOrr <andrew.d.orr@colorado.edu>
2019-06-27 01:33:50 -0500

    string buffers!

--- main.go
+++ main.go
@@ -1,36 +1,46 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
-	fileStats, err := findDirPath("C:/Go")
-	if err != nil {
-		fmt.Errorf("Failed to read file information", err)
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Println("Please specify a directory.")
+		return
 	}
-	js, err := json.MarshalIndent(fileStats, "", "    ")
-	if err != nil {
-		fmt.Errorf("Failed to create json interpretation of map", err)
+	var err error
+	var sb strings.Builder
+	sb.WriteString("{\n\t")
+	sb.WriteString(`"files":`)
+	sb.WriteString("\n\t[\n")
+	for _, dir := range args {
+		err = findDirPath(dir, &sb)
+		if err != nil {
+			fmt.Println(fmt.Errorf("Failed to read file information", err).Error())
+		}
 	}
-	fmt.Println(string(js))
+	sb.WriteString("\t],\n}")
+	fmt.Println(sb.String())
 
 }
 
-func findDirPath(dir string) (map[string]int64, error) {
-	fileStats := make(map[string]int64)
+func findDirPath(dir string, sb *strings.Builder) error {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		return nil, err
+		return err
 	}
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		fileStats[path] = info.Size()
+		sb.WriteString("\t ")
+		sb.WriteString(fmt.Sprintf(`{"%s", %d},`, path, int(info.Size())))
+		sb.WriteString("\n")
 		return nil
 	})
-	return fileStats, err
+	return err
 }
